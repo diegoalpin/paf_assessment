be0e0a782c632b94a149227bfc71184bdc57adff
diegoalpin <diego_alpin@live.com>
2023-03-03 09:51:21 +0800

    first commit, task 1 and 2 finished. Able to run spring boot, connect to MYSQL in railway

---
+++ src/main/java/sg/edu/nus/iss/app/paf_day30assessment/PafDay30AssessmentApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app.paf_day30assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay30AssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay30AssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/app/paf_day30assessment/PafDay30AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app.paf_day30assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay30AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
